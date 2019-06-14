78399d3ac3d65f57b2435b740b2d12cf36a99771
rlatkddn212 <rlatkddn212@chromium.org>
2019-06-14 15:06:13 +0900

    삭제

--- OpenGL_Project/C05_Attrb/vertex.glsl
+++
@@ -1,8 +0,0 @@
-#version 420 core
-
-layout (location = 0) in vec4 position;
-
-void main(void)
-{
-	gl_Position = vec4(position.xyz, 1.0); 
-}
--- OpenGL_Project/C05_Attrib/Image.glsl
+++
@@ -1,8 +0,0 @@
-#version 430 core
-out vec4 color;
-in vec4 vs_color;
-
-void main()
-{
-		color = vs_color;
-}
